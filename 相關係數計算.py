470ae89717ef754ea01824a4963c570f2f96910c
Will150143 <133984319+Will150143@users.noreply.github.com>
2024-07-15 22:46:20 +0800

    Update 相關係數計算.py

--- 相關係數計算.py
+++ 相關係數計算.py
@@ -14,25 +14,22 @@ df['Time'] = pd.to_datetime(df['時間'], errors='coerce').dt.time
 start_time = pd.to_datetime('13:01:00').time()
 end_time = pd.to_datetime('13:30:00').time()
 
-# 篩選時間範圍內的數據
+# 篩選時間範圍
 timefiltered_df = df[(df['Time'] >= start_time) & (df['Time'] <= end_time)]
 
-# 篩選 '價性' 為 "價內" 的數據
-ksdf = timefiltered_df[timefiltered_df['價性'] == "價內 (ITM)"]
-
-# 篩選 '型態' 為 "買權" 的數據
-optiondf = ksdf[ksdf['買賣權'] == "P"].copy()
+# 篩選 '價性' & '類型'
+ksdf = timefiltered_df[(timefiltered_df['價性'] == "深度價內 (DITM)") & (timefiltered_df['買賣權'] == "C") ].copy()
 
 # 計算 '選擇權成交價變化率' 和 '期貨價格變化率' 的相關係數並四捨五入到小數第四位
-correlationTXF = optiondf['選擇權成交價變化率'].corr(optiondf['期貨價格變化率'])
+correlationTXF = ksdf['選擇權成交價變化率'].corr(ksdf['期貨價格變化率'])
 roundedTXF = round(correlationTXF, 4)
 
 # 計算 '選擇權成交價變化率' 和 '指數價格變化率' 的相關係數並四捨五入到小數第四位
-correlationTW = optiondf['選擇權成交價變化率'].corr(optiondf['指數價格變化率'])
+correlationTW = ksdf['選擇權成交價變化率'].corr(ksdf['指數價格變化率'])
 roundedTW = round(correlationTW, 4)
 
 # 計算 '期貨價格變化率' 和 '指數價格變化率' 的相關係數並四捨五入到小數第四位
-correlationTXFTW = timefiltered_df['期貨價格變化率'].corr(timefiltered_df['指數價格變化率'])
+correlationTXFTW = ksdf['期貨價格變化率'].corr(ksdf['指數價格變化率'])
 roundedTXFTW = round(correlationTXFTW, 4)
 
 #計算資料筆數
