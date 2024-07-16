818458abc478c710a1d6193bfb7e1f34ac3f8a6c
Will150143 <133984319+Will150143@users.noreply.github.com>
2024-07-16 08:15:07 +0800

    Update 相關係數計算.py

--- 相關係數計算.py
+++ 相關係數計算.py
@@ -18,27 +18,42 @@ end_time = pd.to_datetime('09:10:00').time()
 timefiltered_df = df[(df['Time'] >= start_time) & (df['Time'] <= end_time)]
 
 # 篩選 '價性' & '類型'
-ksdf = timefiltered_df[(timefiltered_df['價性'] == "深度價內 (DITM)") & (timefiltered_df['買賣權'] == "C")].copy()
+ks="深度價外 (DOTM)"
+option_C="C"
+option_P="P"
+ksdf_C = timefiltered_df[(timefiltered_df['價性'] == ks) & (timefiltered_df['買賣權'] == option_C)].copy()
+ksdf_P = timefiltered_df[(timefiltered_df['價性'] == ks) & (timefiltered_df['買賣權'] == option_P)].copy()
 
 # 計算 '選擇權成交價變化率' 和 '期貨價格變化率' 的相關係數並四捨五入到小數第四位，略過空值
-correlationTXF = ksdf['選擇權成交價變化率'].corr(ksdf['期貨價格變化率'])
-roundedTXF = round(correlationTXF, 4)
+correlationTXF_C = ksdf_C['選擇權成交價變化率'].corr(ksdf_C['期貨價格變化率'])
+roundedTXF_C = round(correlationTXF_C, 4)
+correlationTXF_P = ksdf_P['選擇權成交價變化率'].corr(ksdf_P['期貨價格變化率'])
+roundedTXF_P = round(correlationTXF_P, 4)
 
 # 計算 '選擇權成交價變化率' 和 '指數價格變化率' 的相關係數並四捨五入到小數第四位，略過空值
-correlationTW = ksdf['選擇權成交價變化率'].corr(ksdf['指數價格變化率'])
-roundedTW = round(correlationTW, 4)
+correlationTW_C = ksdf_C['選擇權成交價變化率'].corr(ksdf_C['指數價格變化率'])
+roundedTW_C = round(correlationTW_C, 4)
+correlationTW_P = ksdf_P['選擇權成交價變化率'].corr(ksdf_P['指數價格變化率'])
+roundedTW_P = round(correlationTW_P, 4)
 
 # 計算 '期貨價格變化率' 和 '指數價格變化率' 的相關係數並四捨五入到小數第四位，略過空值
 correlationTXFTW = timefiltered_df['期貨價格變化率'].corr(timefiltered_df['指數價格變化率'])
 roundedTXFTW = round(correlationTXFTW, 4)
 
 # 計算資料筆數
-TX_rows = ksdf['選擇權成交價變化率'].notna().sum()-1
-TXFTW_rows = timefiltered_df['指數價格變化率'].notna().sum()-1
+rows_C = ksdf_C['選擇權成交價變化率'].notna().sum()-1
+rows_P = ksdf_P['選擇權成交價變化率'].notna().sum()-1
+rows_TXFTW = timefiltered_df['指數價格變化率'].notna().sum()-1
 
 # 顯示相關係數 & 資料數
-print(f"{roundedTXF}")
-print(f"{roundedTW}")
-print(f"各{TX_rows}")
+print(ks,option_C)
+print(f"{roundedTXF_C}")
+print(f"{roundedTW_C}")
+print(f"{rows_C}")
+print(ks,option_P)
+print(f"{roundedTXF_P}")
+print(f"{roundedTW_P}")
+print(f"{rows_P}")
+print("加權期貨")
 print(f"{roundedTXFTW}")
-print(f"各{TXFTW_rows}")
+print(f"{rows_TXFTW}")
