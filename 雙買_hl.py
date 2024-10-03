7a15c404603fedb32b3cdeac81e7de008d378335
alfredo <petercy32@gmail.com>
2024-10-03 12:27:35 +0800

    平行

--- 雙買_hl.py
+++ 雙買_hl.py
@@ -16,6 +16,7 @@ from back_test_util.option_action import Option
 from concurrent.futures import ThreadPoolExecutor
 import queue
 
+
 # 忽略 SettingWithCopyWarning 警告
 pd.options.mode.chained_assignment = None
 
@@ -46,24 +47,29 @@ global profit_total
 profit_total = 0
 CorP = 'C'
 hedge_frequency = 10
-
+# stop_loss = 0.5
 
 # record_df = pd.DataFrame(columns=['時間','BorS','履約價','CorP','價格','數量','手續費','損益','損益累計'])
 
 
 record_list =[]
 
-    
+
 def find_most_itm_option(future_price):
     itm_strike_price = future_price -future_price%50
     return itm_strike_price
-
 def find_call_near(price):
-    strike_price = price -price%50
+    if price%50>25:
+        strike_price = price +50 -price%50
+    elif price%50<=25:
+        strike_price = price -price%50
     return strike_price
 
 def find_put_near(price):
-    strike_price = price+50 -price%50
+    if price%50>25:
+        strike_price = price -price%50
+    elif price%50<=25:
+        strike_price = price+50 -price%50
     return strike_price
     
 
@@ -209,7 +215,7 @@ def process_row(index, row, end_date_df, result_queue):
                         
                         
                     # 50%就出場 
-                    if position['價格']*0.5>=position['現價']:
+                    if position['價格']*stop_loss>=position['現價']:
                         if position['BorS'] == 'B':
                             option.option_trade(future_row['時間'], 'S', position['CorP'], position['履約價'], position['現價'], 1, 0, position_action='close')
                         else:
@@ -242,102 +248,102 @@ def process_row(index, row, end_date_df, result_queue):
     }         
     result_queue.put((index, record_dict))
     
-    
-# 使用多线程并行处理
-result_queue = queue.Queue()
-with ThreadPoolExecutor(max_workers=16) as executor:
-    futures = [executor.submit(process_row, index, row, end_date_df, result_queue) for index, row in end_date_df.iterrows()]
-    for future in futures:
-        future.result()
-# 按顺序获取结果
-record_list = [None] * len(end_date_df)
-while not result_queue.empty():
-    index, record_dict = result_queue.get()
-    record_list[index] = record_dict
+for stop_loss in [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8]:
+    # 使用多线程并行处理
+    result_queue = queue.Queue()
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        futures = [executor.submit(process_row, index, row, end_date_df, result_queue) for index, row in end_date_df.iterrows()]
+        for future in futures:
+            future.result()
+    # 按顺序获取结果
+    record_list = [None] * len(end_date_df)
+    while not result_queue.empty():
+        index, record_dict = result_queue.get()
+        record_list[index] = record_dict
 
-# 移除 None 值
-record_list = [record for record in record_list if record is not None]
+    # 移除 None 值
+    record_list = [record for record in record_list if record is not None]
 
 
 
-profit_list = []
-for index,record in enumerate(record_list):
-    profit = 0
-    # record = record_list[1]
-    if record['期貨紀錄'] != []:
-        profit +=record['期貨紀錄'][-1]['損益累計']
-    if record['選擇權紀錄'] != []:
-        profit +=record['選擇權紀錄'][-1]['損益累計']
+    profit_list = []
+    for index,record in enumerate(record_list):
+        profit = 0
+        # record = record_list[1]
+        if record['期貨紀錄'] != []:
+            profit +=record['期貨紀錄'][-1]['損益累計']
+        if record['選擇權紀錄'] != []:
+            profit +=record['選擇權紀錄'][-1]['損益累計']
 
-    profit_list.append(profit)
+        profit_list.append(profit)
 
-profit_list = [float(profit) for profit in profit_list]
+    profit_list = [float(profit) for profit in profit_list]
 
-# 統計
-statistic_data_dict ={
-    '獲利次數':sum(1 for profit in profit_list if profit > 0),
-    '虧損次數':sum(1 for profit in profit_list if profit <= 0),
-    '獲利金額':sum(profit for profit in profit_list if profit > 0),
-    '虧損金額':sum(profit for profit in profit_list if profit <=0),
-}
+    # 統計
+    statistic_data_dict ={
+        '獲利次數':sum(1 for profit in profit_list if profit > 0),
+        '虧損次數':sum(1 for profit in profit_list if profit <= 0),
+        '獲利金額':sum(profit for profit in profit_list if profit > 0),
+        '虧損金額':sum(profit for profit in profit_list if profit <=0),
+    }
 
-statistic_dict = {
-    '期間':f'{record_list[0]["日期"]}~{record_list[-1]["日期"]}',
-    '總淨利':statistic_data_dict['獲利金額']+statistic_data_dict['虧損金額'],
-    '毛利':statistic_data_dict['獲利金額'],
-    '毛損失':statistic_data_dict['虧損金額'],
-    '總交易天數':len(profit_list),
-    '勝率':statistic_data_dict['獲利次數']/(statistic_data_dict['獲利次數']+statistic_data_dict['虧損次數']),
-    
-    '成功筆數':statistic_data_dict['獲利次數'],
-    '失敗筆數':statistic_data_dict['虧損次數'],
-    '最大獲利交易':max(profit_list),
-    '最大虧損交易':min(profit_list),
-    '成功交易平均獲利':statistic_data_dict['獲利金額']/statistic_data_dict['獲利次數'],
-    '失敗交易平均虧損':statistic_data_dict['虧損金額']/statistic_data_dict['虧損次數'],
-    '平均獲利/平均虧損':(statistic_data_dict['獲利金額']/statistic_data_dict['獲利次數'])/(statistic_data_dict['虧損金額']/statistic_data_dict['虧損次數']),
-    '平均每筆交易盈虧':(statistic_data_dict['獲利金額']+statistic_data_dict['虧損金額'])/(statistic_data_dict['獲利次數']+statistic_data_dict['虧損次數']),
-}
-
-# 將 record_list 轉換為 DataFrame
-with pd.ExcelWriter(f'record_list_9hl_{hedge_frequency}_{record_list[0]["日期"]}-{record_list[-1]["日期"]}.xlsx') as writer:
-    statistic_df = pd.DataFrame(statistic_dict, index=[0])
-    statistic_df.transpose().to_excel(writer, sheet_name='統計', header=False)
-    for record in record_list:
-        # record = record_list[0]
-        # 將每個記錄轉換為 DataFrame
-        df = pd.DataFrame()
-        if record['期貨紀錄'] == []:
-            df_future = pd.DataFrame(columns=['時間_期貨紀錄','收盤價_期貨紀錄','數量_期貨紀錄','手續費_期貨紀錄','損益_期貨紀錄','損益累計_期貨紀錄'])
-        else:
-            df_future = pd.DataFrame(record['期貨紀錄']).add_suffix('_期貨紀錄')
+    statistic_dict = {
+        '期間':f'{record_list[0]["日期"]}~{record_list[-1]["日期"]}',
+        '總淨利':statistic_data_dict['獲利金額']+statistic_data_dict['虧損金額'],
+        '毛利':statistic_data_dict['獲利金額'],
+        '毛損失':statistic_data_dict['虧損金額'],
+        '總交易天數':len(profit_list),
+        '勝率':statistic_data_dict['獲利次數']/(statistic_data_dict['獲利次數']+statistic_data_dict['虧損次數']),
         
-        if record['選擇權紀錄'] == []:
-            df_option = pd.DataFrame(columns=['時間_選擇權紀錄','BorS_選擇權紀錄','倉位狀況_選擇權紀錄','履約價_選擇權紀錄','CorP_選擇權紀錄','價格_選擇權紀錄','數量_選擇權紀錄','手續費_選擇權紀錄','損益_選擇權紀錄','損益累計_選擇權紀錄'])
-        else:
-            df_option = pd.DataFrame(record['選擇權紀錄']).add_suffix('_選擇權紀錄')
-        
-        record['call_價格'] = record['call_價格'].add_suffix('_call')
-        record['put_價格'] = record['put_價格'].add_suffix('_put')
-        
-        df = pd.merge(record['加權指數'],record['期貨'], left_on='分鐘時間',right_on='時間', how='inner', suffixes=('_加權指數', '_期貨'))
-        df = df[['時間','收盤價_加權指數','收盤價_期貨']]
-        df = pd.merge(df,df_option, left_on='時間',right_on='時間_選擇權紀錄', how='left', suffixes=('', '_選擇權紀錄'))
-        df = pd.merge(df,df_future, left_on='時間',right_on='時間_期貨紀錄', how='left', suffixes=('', '_期貨紀錄'))
-        
-        df = pd.merge(df,record['call_價格'], left_on='時間',right_on='時間_call', how='left', suffixes=('', '_call'))
-        df = pd.merge(df,record['put_價格'], left_on='時間',right_on='時間_put', how='left', suffixes=('', '_put'))
-        
-        df.sort_values(by='時間', inplace=True)
-        
-        # 使用日期作為分頁名稱
-        sheet_name = record['日期']
-        
-        
-        # 將 DataFrame 寫入 Excel 分頁
-        df.to_excel(writer, sheet_name=sheet_name, index=False)
+        '成功筆數':statistic_data_dict['獲利次數'],
+        '失敗筆數':statistic_data_dict['虧損次數'],
+        '最大獲利交易':max(profit_list),
+        '最大虧損交易':min(profit_list),
+        '成功交易平均獲利':statistic_data_dict['獲利金額']/statistic_data_dict['獲利次數'],
+        '失敗交易平均虧損':statistic_data_dict['虧損金額']/statistic_data_dict['虧損次數'],
+        '平均獲利/平均虧損':(statistic_data_dict['獲利金額']/statistic_data_dict['獲利次數'])/(statistic_data_dict['虧損金額']/statistic_data_dict['虧損次數']),
+        '平均每筆交易盈虧':(statistic_data_dict['獲利金額']+statistic_data_dict['虧損金額'])/(statistic_data_dict['獲利次數']+statistic_data_dict['虧損次數']),
+    }
 
-print("record_list 已保存為 record_list.xlsx")
+    # 將 record_list 轉換為 DataFrame
+    with pd.ExcelWriter(f'record_list_9hl_{stop_loss}_{hedge_frequency}_{record_list[0]["日期"]}-{record_list[-1]["日期"]}.xlsx') as writer:
+        statistic_df = pd.DataFrame(statistic_dict, index=[0])
+        statistic_df.transpose().to_excel(writer, sheet_name='統計', header=False)
+        for record in record_list:
+            # record = record_list[0]
+            # 將每個記錄轉換為 DataFrame
+            df = pd.DataFrame()
+            if record['期貨紀錄'] == []:
+                df_future = pd.DataFrame(columns=['時間_期貨紀錄','收盤價_期貨紀錄','數量_期貨紀錄','手續費_期貨紀錄','損益_期貨紀錄','損益累計_期貨紀錄'])
+            else:
+                df_future = pd.DataFrame(record['期貨紀錄']).add_suffix('_期貨紀錄')
+            
+            if record['選擇權紀錄'] == []:
+                df_option = pd.DataFrame(columns=['時間_選擇權紀錄','BorS_選擇權紀錄','倉位狀況_選擇權紀錄','履約價_選擇權紀錄','CorP_選擇權紀錄','價格_選擇權紀錄','數量_選擇權紀錄','手續費_選擇權紀錄','損益_選擇權紀錄','損益累計_選擇權紀錄'])
+            else:
+                df_option = pd.DataFrame(record['選擇權紀錄']).add_suffix('_選擇權紀錄')
+            
+            record['call_價格'] = record['call_價格'].add_suffix('_call')
+            record['put_價格'] = record['put_價格'].add_suffix('_put')
+            
+            df = pd.merge(record['加權指數'],record['期貨'], left_on='分鐘時間',right_on='時間', how='inner', suffixes=('_加權指數', '_期貨'))
+            df = df[['時間','收盤價_加權指數','收盤價_期貨']]
+            df = pd.merge(df,df_option, left_on='時間',right_on='時間_選擇權紀錄', how='left', suffixes=('', '_選擇權紀錄'))
+            df = pd.merge(df,df_future, left_on='時間',right_on='時間_期貨紀錄', how='left', suffixes=('', '_期貨紀錄'))
+            
+            df = pd.merge(df,record['call_價格'], left_on='時間',right_on='時間_call', how='left', suffixes=('', '_call'))
+            df = pd.merge(df,record['put_價格'], left_on='時間',right_on='時間_put', how='left', suffixes=('', '_put'))
+            
+            df.sort_values(by='時間', inplace=True)
+            
+            # 使用日期作為分頁名稱
+            sheet_name = record['日期']
+            
+            
+            # 將 DataFrame 寫入 Excel 分頁
+            df.to_excel(writer, sheet_name=sheet_name, index=False)
+
+    print("record_list 已保存為 record_list.xlsx")
 
 
 
